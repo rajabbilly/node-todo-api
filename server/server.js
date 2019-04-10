a8c01b7b0f0578060036db8f90e635ea2d3b6221
Rajab Billy <rajabbilly@gmail.com>
2019-04-10 07:52:33 +0200

    added a return key word

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get("/todos/:id", (req, res) => {
   Todo.findById(id)
     .then(todo => {
       if (!todo) {
-        res.status(404).send();
+        return res.status(404).send();
       }
       res.status(200).send({ todo });
     })
