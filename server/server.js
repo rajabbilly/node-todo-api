104d5ad2c51dabbdd84a20f08311582923a69634
Rajab Billy <rajabbilly@gmail.com>
2019-04-12 10:35:54 +0200

    added few changes to GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -47,7 +47,8 @@ app.get("/todos/:id", (req, res) => {
       if (!todo) {
         return res.status(404).send();
       }
-      res.status(200).send({ todo });
+
+      res.send({ todo });
     })
     .catch(e => {
       res.status(400).send();
