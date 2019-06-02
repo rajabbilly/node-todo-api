328bbcf48c43b73854cd9453019f9e1fee6ef3b4
Rajab Billy <rajabbilly@gmail.com>
2019-06-02 09:05:19 +0200

    add post users route and auth token

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 var { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
+var { Users } = require("./models/user");
 
 var app = express();
 const port = process.env.PORT;
@@ -104,6 +104,23 @@ app.patch("/todos/:id", (req, res) => {
     })
     .catch(e => {
       res.status(400).send();
+    });
+});
+
+app.post("/users", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new Users(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
     });
 });
 
